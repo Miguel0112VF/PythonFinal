17adc6e82bed8dd348cd47e795f26fb86538e8da
Miguel0112VF <129977914+Miguel0112VF@users.noreply.github.com>
2023-05-09 15:02:35 -0400

    Add files via upload

---
+++ TestPythonExamFinalTest.py
@@ -0,0 +1,22 @@
+def get_observed_substrings(string):
+    substrings = set()
+    for j in range(1, len(string) + 1):
+        for i in range(0, len(string) - j + 1):
+            substrings.add(string[i:i+j])
+    return substrings
+#giving this code/seuqnce a name 
+
+def get_possible_substrings(string, k):
+    a = 4**k
+    b = len(string) - (k-1)
+    if a > b:
+        return b
+    else:
+        return a
+#giving this code/seuqnce a name 
+    
+def calculate_linguistic_complexity(string):
+    observed_substrings = get_observed_substrings(string)
+    possible_substrings = sum([get_possible_substrings(string, k) for k in range(1, len(string) + 1)])
+    return len(observed_substrings) / possible_substrings
+
